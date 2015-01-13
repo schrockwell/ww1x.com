b26f342ec63a2f912c241047af892659d7d39983
Rockwell Schrock <schrockwell@gmail.com>
2015-01-12 21:50:45 -0800

    Updated WaveGuide for 1.3 with new screenshots, etc.

--- sotagoat/images/screenshots/resize.sh
+++ sotagoat/images/screenshots/resize.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 for i in {1..3} 
 do
-	convert iphone-$i.png -resize 198x372 -sharpen 0x0.5 iphone-$i-medium.png
-	convert iphone-$i.png -resize 74x139 -sharpen 0x0.5 iphone-$i-thumb.png
+	convert iphone-$i-orig.png -resize 396x744 -sharpen 0x0.5 iphone-$i-medium.png
+	convert iphone-$i-orig.png -resize 148x278 -sharpen 0x0.5 iphone-$i-thumb.png
+	convert iphone-$i-orig.png -resize 1024x1024\> -sharpen 0x0.5 iphone-$i.png
 done
 
 for i in {1..2} 
 do
-	convert ipad-$i.png -resize 390x372 -sharpen 0x0.5 ipad-$i-medium.png
-	convert ipad-$i.png -resize 180x139 -sharpen 0x0.5 ipad-$i-thumb.png
+	convert ipad-$i-orig.png -resize 780x744 -sharpen 0x0.5 ipad-$i-medium.png
+	convert ipad-$i-orig.png -resize 360x278 -sharpen 0x0.5 ipad-$i-thumb.png
+	convert ipad-$i-orig.png -resize 1024x1024\> -sharpen 0x0.5 ipad-$i.png
 done
